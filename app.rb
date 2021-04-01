3c736e5a46a7dd9692b2990af386f8bd5482ef7c
Andrés Nuñez <andresanunez98@gmail.com>
2021-04-01 13:23:20 -0400

    commit tree 916047ca0850a919da930105617b1d09e69d7292

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Andres" 
+      end 
+
+      get '/hometown' do
+        "My hometown is Cuernavaca,Morelos,Mexico"
+      end 
+
+      get '/favorite-song' do
+        "My favorite song is True Blue by Angel Olsen"
+      end 
+
 end
+
+
+# name
+# hometown
+# favorite-song
